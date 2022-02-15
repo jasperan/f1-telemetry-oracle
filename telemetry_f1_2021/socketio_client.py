9fa74e8f72fefeb368e52d5d7b5b14ea3c9e7e49
jasperan <23caj23@gmail.com>
2022-02-15 14:05:01 -0500

    inprogress

---
+++ telemetry_f1_2021/socketio_client.py
@@ -0,0 +1,24 @@
+import asyncio
+import socketio
+
+sio = socketio.AsyncClient()
+
+@sio.event
+async def connect():
+    print('connection established')
+
+@sio.event
+async def my_message(data):
+    print('message received with ', data)
+    await sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+async def disconnect():
+    print('disconnected from server')
+
+async def main():
+    await sio.connect('http://localhost:8080')
+    await sio.wait()
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- telemetry_f1_2021/socketio_server.py
+++ telemetry_f1_2021/socketio_server.py
@@ -1,22 +1,19 @@
 import asyncio
 from aiohttp import web
 import socketio
+'''
 import pika
+import eventlet
+import socketio
 
-
-sio = socketio.AsyncServer()
-app = web.Application()
-sio.attach(app)
-
-async def index(request):
-    """Serve the client-side application."""
-    with open('index.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
 
 @sio.event
 def connect(sid, environ):
-    print("connect ", sid)
-    # a
+    print('connect ', sid)
     connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
@@ -32,20 +29,59 @@ def connect(sid, environ):
     # PacketMotionData -> queue
     sio.emit('PacketMotionData', {})
 
-    
-
-
 
 @sio.event
-async def message(sid, data):
+def message(sid, data):
     print("message ", data)
     sio.emit('PacketExample', {'foo': 'bar'})
+
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
-app.router.add_static('/static', 'static')
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)'''
+
+from aiohttp import web
+import socketio
+import pika
+
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+async def index(request):
+    """Serve the client-side application."""
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+    connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    list_packet_types = ['PacketMotionData', 'PacketSessionData', 'PacketLapData', 'PacketEventData', 'PacketParticipantsData',
+        'PacketCarSetupData', 'PacketCarTelemetryData', 'PacketCarStatusData', 'PacketFinalClassificationData', 'PacketLobbyInfoData',
+        'PacketCarDamageData', 'PacketSessionHistoryData']
+
+    # declare all queues
+    for x in list_packet_types:
+        channel.queue_declare(queue='{}'.format(x))
+
+    # PacketMotionData -> queue
+    sio.emit('PacketMotionData', {})
+
+@sio.event
+async def chat_message(sid, data):
+    print("message ", data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
 app.router.add_get('/', index)
 
 if __name__ == '__main__':
