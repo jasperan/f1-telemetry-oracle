acc69a222ec3341eb4b3a0c903510ed4faf22538
jasperan <23caj23@gmail.com>
2022-02-22 19:01:30 +0100

    fix: bugfix

--- telemetry_f1_2021/ws_requester.py
+++ telemetry_f1_2021/ws_requester.py
@@ -1,10 +1,15 @@
 import asyncio
 from websockets import connect
+import time
+
+# Client simulator for web socket connection to a server located in the below mentioned IP address and port. 
 
 async def hello(uri):
     async with connect(uri) as websocket:
-        await websocket.send("getPacketCarTelemetryData")
-        message = await websocket.recv()
-        print(message)
+        while True:
+            await websocket.send("getPacketCarTelemetryData")
+            message = await websocket.recv()
+            print(message)
+            time.sleep(.05)
 
 asyncio.run(hello("ws://130.61.139.189:8001"))
